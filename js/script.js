f130e00d091ec1e848414c555fad19cfc7277068
abelbirhanu1057@gmail.com <113086309+abel-b-degu@users.noreply.github.com>
2023-08-09 07:57:08 -0500

    Updated the calculator buttons including the backspace

--- js/script.js
+++ js/script.js
@@ -4,52 +4,109 @@ let displayValue = '0';
 
 /*DOM elements */
 const display = document.querySelector('#calculator-display');
+const backspaceButton = document.querySelector('#backspace');
+const numberButtons = document.querySelectorAll('.container button[type="button"][value]');
+
+//Backspace button click event listener
+backspaceButton.addEventListener('click', handleBackspace);
+
+//Number button click event listener
+numberButtons.forEach(button => {
+    button.addEventListener('click',() => handleNumberButtonClick(button.value));
+});
+
+//Backspace button click event handler
+function handleBackspace(){
+    if(displayValue.length > 1){
+            displayValue = displayValue.slice(0, -1);
+        }
+        else{
+            displayValue = '0';
+        }
+        updateDisplay();
+    
+}
+
+// function updateDispaly(){
+//     display.innerText = dispalyValue;
+// }
+function handleNumberButtonClick(number) {
+    if (displayValue === '0' || displayValue === 'Error') {
+        displayValue = number;
+    } else {
+        displayValue += number;
+    }
+    updateDisplay();
+}
+
 
 /* Assignment letter a) Math functions */
-
 function add(number1, number2){
-    return number1 + number2;
-};
+    const result = number1 + number2;
+    displayValue = result.toString();
+    updateDisplay();
+    return result;
+}
 
 function subtract(number1, number2){
-    return number1 - number2;
-};
+    const result = number1 - number2;
+    displayValue = result.toString();
+    updateDisplay();
+    return result;
+}
 
 function multiply(number1, number2){
-    return number1 * number2;
-};
+    const result = number1 * number2;
+    displayValue = result.toString();
+    updateDisplay();
+    return result;
+}
 
 function divide(number1, number2){
-    if(number2 === 0){
-        return number1
-    } else{
-    return number1 / number2;
-    };
-};
+    if (number2 === 0) {
+        return number1;
+    } else {
+        const result = number1 / number2;
+        displayValue = result.toString();
+        updateDisplay();
+        return result;
+    }
+}
 
-function percent(number1,number2){
-    return (number1 / 100) * number2;
-};
+function percent(number1, number2){
+    const result = (number1 / 100) * number2;
+    displayValue = result.toString();
+    updateDisplay();
+    return result;
+}
 
-/*Assignmet letter b) operate function */
+/*Assignment letter b) operate function */
 
 function operate(operator, number1, number2){
+    let result;
     switch(operator) {
         case '+':
-            return add(number1, number2);
+            result = add(number1, number2);
             break;
         case '-':
-            return subtract(number1, number2);
+            result = subtract(number1, number2);
             break;
         case '*':
-            return multiply(number1, number2);
+            result = multiply(number1, number2);
             break;
         case '/':
-            return divide(number1, number2);
+            result = divide(number1, number2);
             break;
-    };
-};
+    }
+    return result;
+}
 
 /*Assignmet letter d) display atualization */
 
-display.innerText = displayValue;
+function updateDisplay() {
+    display.innerText = displayValue;
+}
+
+// Initial display update
+updateDisplay();
+
